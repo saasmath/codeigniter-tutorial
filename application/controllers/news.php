5ab6fa7257fa3c990d040bcd834014712d3d6bf0
Gary Anderson <git@garyvictoranderson.com>
2012-09-01 13:24:44 -0700

    Add code to send article to news view.
    This also passes a title into the view.

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -9,6 +9,11 @@ class News extends CI_Controller {
 
     public function index() {
         $data['news'] = $this->news_model->get_news();
+        $data['title'] = 'News archive';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/index', $data);
+        $this->load->view('templates/footer');
     }
 
     public function view($slug) {
